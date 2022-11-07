11417133bbbc09af1fa2067280c64eae69ce7374
maikully <zeebli123@gmail.com>
2022-11-06 21:40:31 -0500

    copy portfolio

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from './logo.svg'
+import './App.css'
+import Button from 'react-bootstrap/Button'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import Homework1 from './Homework1'
+import Homework2 from './Homework2'
+import { useState, Component } from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor () {
+    super()
+    this.state = { show: 0 }
+    this.showHandler = this.showHandler.bind(this)
+  }
+  showHandler = x => {
+    this.setState({ show: x })
+  }
+  render () {
+    return (
+      <div className='App'>
+        <header className='App-header'>
+          <h1>Welcome to my portfolio.</h1>
+          <br></br>
+          <h2>Choose an assignment.</h2>
+          <Button
+            style={{ marginBottom: '1vw', marginTop: '2vw' }}
+            onClick={() => this.showHandler(1)}
+          >
+            Personas & Storyboarding
+          </Button>
+          <Button
+            style={{ marginBottom: '1vw' }}
+            onClick={() => this.showHandler(2)}
+          >
+            Responsive Redesign
+          </Button>
+          <Button
+            variant='secondary'
+            style={{ marginBottom: '3vw' }}
+            onClick={() => this.showHandler(0)}
+          >
+            Reset
+          </Button>
+          {this.state.show != 0 && <h4>scroll down!</h4>}
+        </header>
+        {this.state.show == 1 && <Homework1 />}
+        {this.state.show == 2 && <Homework2 />}
+      </div>
+    )
+  }
 }
 
-export default App;
+export default App
---
+++ src/FadeSection.js
@@ -0,0 +1,34 @@
+import logo from './logo.svg'
+import './App.css'
+import Button from 'react-bootstrap/Button'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import React from 'react'
+
+function FadeSection(prop) {
+  // source: https://stackoverflow.com/questions/59595700/how-to-make-a-react-component-fade-in-on-scroll-using-intersectionobserver-but
+  const [isVisible, setVisible] = React.useState(false);
+  const domRef = React.useRef();
+  React.useEffect(() => {
+    console.log(prop)
+    const observer = new IntersectionObserver(entries => {
+      entries.forEach(entry => {
+        if (entry.isIntersecting) {
+          setVisible(true);
+          //observer.unobserve(domRef.current);
+        }
+      });
+    });
+    observer.observe(domRef.current);
+    return () => observer;
+  }, []);
+  return (
+    <div
+      className={`fade-in-section ${isVisible ? 'is-visible' : ''}`}
+      ref={domRef}
+    >
+      {prop.children}
+    </div>
+  );
+}
+
+export default FadeSection
